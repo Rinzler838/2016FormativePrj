b06720c4d9d2e0139f19a4fad274073ab2ed1228
Rinzler838 <dvil4688@740s-pl-105.local>
2016-03-23 07:56:17 -0600

    Created project for the 2016 Formative, added Runner

---
+++ src/formative/controller/CharacterRunner.java
@@ -0,0 +1,12 @@
+package formative.controller;
+
+import formative.controller.CharacterController;
+
+public class CharacterRunner
+{
+	public static void main(String[] args)
+	{
+		CharacterController myController = new CharacterController();
+		myController.start();
+	}
+}
